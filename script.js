029ff771c9dea46f5977d68af4fb96b76aefc587
NIYATI1-CODE <niyatisingh629@gmail.com>
2026-08-05 06:52:15 +0000

    Implement dynamic grid resizing

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
 const container = document.querySelector('.container');
-
-for (let i = 0; i < 256; i++) {
-  const square = document.createElement('div');
-  square.classList.add("square");
-  square.addEventListener('mouseover', () => {
-    square.classList.add('change');
-
+grid(16);
+function grid(no_square) {
+  container.innerHTML = "";
+  let total;
+  if (no_square <= 100) {
+    if (!no_square) {
+      total = 256;
+    }
+    else {
+      total = no_square * no_square;
+    }
   }
-  )
-  container.appendChild(square);
+  else {
+    alert('Invalid number. Enter less than 100');
+    return;
+  }
+
+  for (let i = 0; i < total; i++) {
+    const square = document.createElement('div');
+    square.classList.add("square");
+    const size = 560 / no_square ;
+    square.style.width = `${size}px`;
+    square.style.height = `${size}px`;
+
+    square.addEventListener('mouseover', () => {
+      square.classList.add('change');
+    }
+    )
+    container.appendChild(square);
+  }
 }
 
-
+const new_grid = document.querySelector('.new_grid');
+new_grid.addEventListener('click', () => {
+  let result = prompt('Enter new number of square per side(less than 100):', '18');
+  grid(result);
+})
