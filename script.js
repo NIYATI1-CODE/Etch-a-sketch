0cbb2a98bc19e8a1024944b0482e05c230dfc0e4
NIYATI1-CODE <niyatisingh629@gmail.com>
2026-08-05 06:14:54 +0000

    Set up initial Etch-a-Sketch project

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector('.container');
+
+for (let i = 0; i < 256; i++) {
+  const square = document.createElement('div');
+  square.classList.add("square");
+  square.addEventListener('mouseover', () => {
+    square.classList.add('change');
+
+  }
+  )
+  container.appendChild(square);
+}
+
+
